99ad917c6b70dabcedc5efc817539fb74281c770
Joseph da costa ribeiro <87831574+josephDcostaR@users.noreply.github.com>
2025-05-24 11:41:06 -0300

    properties, game, orm

---
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -0,0 +1,69 @@
+package com.devsuperior.dslist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_game")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String title;
+
+    @Column(name = "game_year")
+    private Integer year;
+
+    private String genre;
+    private String platform;
+    private Double score;
+    private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
+    private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
+    private String longDescription;
+
+    public Game(){
+        
+    }
+
+    public Game(long id, String title, Integer year, String genre, String platform, Double score, String imgUrl,
+            String shortDescription, String longDescription) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.platform = platform;
+        this.score = score;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Game game = (Game) o;
+        return id == game.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    
+    
+
+}
